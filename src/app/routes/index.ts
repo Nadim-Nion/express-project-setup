67503df42bbdc5bfb719e027cb4b9af6808b4aac
Nadim-Nion <nadim15-1746@diu.edu.bd>
2026-04-12 13:34:03 +0600

    routes and middlewares added

---
+++ src/app/routes/index.ts
@@ -0,0 +1,19 @@
+// import { Router } from 'express';
+
+// const router = Router();
+
+// const moduleRoutes = [
+//   {
+//     path: '/users',
+//     route: UserRoute,
+//   },
+// ];
+
+// /* router.use('/users', UserRoutes);
+// router.use('/students', StudentRoutes); */
+
+// moduleRoutes.forEach((moduleRoute) =>
+//   router.use(moduleRoute.path, moduleRoute.route),
+// );
+
+// export default router;
